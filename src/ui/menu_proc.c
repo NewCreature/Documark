298b5fe39d9f7e2bfe39535fea03e7814fd733ab
Todd Cope <todd@t3-i.com>
2021-10-18 12:34:15 -0500

    Implement creating a new document.

--- src/ui/menu_proc.c
+++ src/ui/menu_proc.c
@@ -1,5 +1,23 @@
+#include "t3f/t3f.h"
+#include "../instance.h"
+
 int dm_menu_file_new(int id, void * data)
 {
+	APP_INSTANCE * app = (APP_INSTANCE *)data;
+	ALLEGRO_FILECHOOSER * fc;
+
+	fc = al_create_native_file_dialog(NULL, "Choose Base Image File", "*.png;*.jpg;*.bmp;*.tga", ALLEGRO_FILECHOOSER_FILE_MUST_EXIST);
+	if(al_show_native_file_dialog(t3f_display, fc))
+	{
+		if(al_get_native_file_dialog_count(fc))
+		{
+			if(app->document)
+			{
+				dm_destroy_document(app->document);
+			}
+			app->document = dm_create_document(al_get_native_file_dialog_path(fc, 0));
+		}
+	}
 	return 0;
 }
 
